bffe683a1503a977b1a4ea4eb7a4c451d39c7699
Agnes Kuus <agnes@mindly.ee>
2019-10-22 23:38:52 +0300

    Implement base architecture

--- Sources/App/Setup/routes.swift
+++ Sources/App/Setup/routes.swift
@@ -1,17 +1,21 @@
 import Vapor
 import Data
 import OpenApi
+import LawHierarchy
 
 /// Register your application's routes here.
 public func routes(_ router: Router, _ container: Container) throws {
     // Route Groups
     let emptyRouteGroup = ""
     let v1RouteGroup: [PathComponentsRepresentable] =  ["api", "v1"]
-
+    
+    let lawService = try container.make(LawService.self)
+    let lawController = LawController(lawService: lawService, group: v1RouteGroup)
 
     let controllers: [RouteCollection] = [
         ApiSpecController(group: emptyRouteGroup),
         ReDocController(group: emptyRouteGroup),
+        lawController
     ]
 
     try controllers.forEach { try router.register(collection: $0) }
