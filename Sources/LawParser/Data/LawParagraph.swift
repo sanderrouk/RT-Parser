c956a3bfdcf5c1a87e400d0e6eaba3274776ad4f
Agnes Kuus <agneskuus@gmail.com>
2020-01-05 21:48:18 +0200

    RTP-7 - Add displayableNumber field

--- Sources/LawParser/Data/LawParagraph.swift
+++ Sources/LawParser/Data/LawParagraph.swift
@@ -2,9 +2,10 @@ import Vapor
 
 public struct LawParagraph: Content {
     let id: String
-    let index: String?
+    let index: Int?
     let number: Int
     let title: String
     let sections: [LawSection]?
     let content: String?
+    let displayableNumber: String
 }
